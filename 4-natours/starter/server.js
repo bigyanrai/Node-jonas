d5bc2844b67b797eae0bb7ab1fd3a2f317865ae0
bigyanrai <9813199661now@gmail.com>
2024-08-08 12:11:25 +0545

    created a mehod on server.js to connect to localhost or remote atlas database

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,5 @@
 //START SERVER
+const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
@@ -6,6 +7,22 @@ dotenv.config({
   path: './config.env',
 });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// const DB = process.env.DATABASE_LOCAL;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection succesfull');
+  });
 //app must be imported before configuring .env file in the process.env using dotenv package
 const app = require('./app');
 // process.env.USERNAME = process.env.USERNAME || 'bigyan';
