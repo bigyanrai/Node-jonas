111073dd92ab46774f4523a484ec4c31b8939413
bigyanrai <9813199661now@gmail.com>
2024-08-12 15:28:59 +0545

    Update the tour model

--- 4-natours/starter/controller/tourController.js
+++ 4-natours/starter/controller/tourController.js
@@ -74,23 +74,43 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
+      message: error,
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      satuts: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
       message: 'Invalid data sent',
     });
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    satuts: 'success',
-    data: {
-      tour: '<updated tour here....>',
-    },
-  });
-};
+exports.deleteTour = async (req, res) => {
+  try {
+    const result = await Tour.findByIdAndDelete(req.params.id);
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    satuts: 'success',
-    data: null,
-  });
+    res.status(200).json({
+      satuts: 'success',
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -7,14 +7,51 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have groupsize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty'],
+  },
+
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have imagecover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 //MODEL
